81fa207401607eb5f7128667f9a6257b7cc1d19a
yasminedaly <yasminedaly@esprit.tn>
2020-03-05 22:40:00 +0100

    ajout d'un ennemi.h

---
+++ ennemi.h
@@ -0,0 +1,13 @@
+#ifndef ENNEMI_H_
+#define ENNEMI_H_
+typedef struct ennemis
+{
+	SDL_Rect pos1;
+	SDL_Rect pos2;
+	SDL_Surface *e1;
+	SDL_Surface *e2;
+	SDL_Surface *e3;
+	SDL_Surface *enb;
+}ennemis;
+
+void initialiserennemi(ennemis *ennemi);
