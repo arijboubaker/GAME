2d9ba0a599ce4c7034e07cbcca34c816c02ac663
hichem-hammami <61579087+hichem-hammami@users.noreply.github.com>
2020-03-23 15:44:33 +0100

    Delete scrolling.h

--- scrolling.h
+++
@@ -1,7 +0,0 @@
-typedef struct background{
-	SDL_Surface *background;
-	SDL_Rect posbg;
-	int dx,fx,dy,fy;
-}background;
-void aff_bg(SDL_Surface *ecran,SDL_Surface *bg,SDL_Rect posbg);
-void scrolling(SDL_Surface *ecran,SDL_Rect posperso,SDL_Rect posbg,int dx,int dy,int fx,int fy,int direction,int d,int k);
